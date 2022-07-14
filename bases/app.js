dfb78da7fdc2508a7846130528bc33648ef2f7cc
Tomas Cuevas <memo_mister398@hotmail.com>
2022-07-14 08:50:49 -0300

    Tipo Any

--- bases/app.js
+++ bases/app.js
@@ -2,11 +2,10 @@
 const a = 10;
 let b = 10;
 function sayHello(msg) {
-  console.log("Bienvenido " + msg + ".");
+    console.log("Bienvenido " + msg + ".");
 }
 sayHello("Tomas");
-
 (() => {
-  const a = 50;
-  console.log(a);
+    const a = 50;
+    console.log(a);
 })();
