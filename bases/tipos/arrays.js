fb4d1ee23ca32195e6622c0fb13f378f7987b8fa
Tomas Cuevas <memo_mister398@hotmail.com>
2022-07-14 08:59:28 -0300

    Arrays - Arreglos

---
+++ bases/tipos/arrays.js
@@ -0,0 +1,7 @@
+"use strict";
+(() => {
+    const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const villans = ["Omega Rojo", "Dormamu", "Duende Verde"];
+    villans.forEach((villan) => console.log(villan.toUpperCase()));
+    console.log(numbers);
+})();
