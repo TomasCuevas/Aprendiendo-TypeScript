443b0bc76fc1eaa07e83abdf0da7d1165d9d8564
Tomas Cuevas <memo_mister398@hotmail.com>
2022-07-14 10:06:27 -0300

    Tarea y Resolución del Ejercicio #1

--- bases/app.js
+++ bases/app.js
@@ -1,11 +1,35 @@
 "use strict";
-const a = 10;
-let b = 10;
-function sayHello(msg) {
-    console.log("Bienvenido " + msg + ".");
-}
-sayHello("Tomas");
 (() => {
-    const a = 50;
-    console.log(a);
+    // Tipos
+    const batman = "Bruce";
+    const superman = "Clark";
+    const existe = false;
+    // Tuplas
+    const parejaHeroes = [batman, superman];
+    const villano = ["Lex Lutor", 5, true];
+    // Arreglos
+    const aliados = ["Mujer Maravilla", "Acuaman", "San", "Flash"];
+    //Enumeraciones
+    let strength;
+    (function (strength) {
+        strength[strength["acuaman"] = 0] = "acuaman";
+        strength[strength["batman"] = 1] = "batman";
+        strength[strength["flash"] = 5] = "flash";
+        strength[strength["superman"] = 100] = "superman";
+    })(strength || (strength = {}));
+    const fuerzaFlash = strength.flash;
+    const fuerzaSuperman = strength.superman;
+    const fuerzaBatman = strength.batman;
+    const fuerzaAcuaman = strength.acuaman;
+    // Retorno de funciones
+    function activar_batiseñal() {
+        return "activada";
+    }
+    function pedir_ayuda() {
+        console.log("Auxilio!!!");
+    }
+    // Aserciones de Tipo
+    const poder = "100";
+    const largoDelPoder = poder.length;
+    console.log(largoDelPoder);
 })();
--- bases/app.ts
+++ bases/app.ts
@@ -1,13 +1,41 @@
-const a: Number = 10;
-let b: Number = 10;
-
-function sayHello(msg: String) {
-  console.log("Bienvenido " + msg + ".");
-}
-
-sayHello("Tomas");
-
 (() => {
-  const a: Number = 50;
-  console.log(a);
+  // Tipos
+  const batman: string = "Bruce";
+  const superman: string = "Clark";
+
+  const existe: boolean = false;
+
+  // Tuplas
+  const parejaHeroes: [string, string] = [batman, superman];
+  const villano: [string, number, boolean] = ["Lex Lutor", 5, true];
+
+  // Arreglos
+  const aliados: string[] = ["Mujer Maravilla", "Acuaman", "San", "Flash"];
+
+  //Enumeraciones
+  enum strength {
+    acuaman = 0,
+    batman = 1,
+    flash = 5,
+    superman = 100,
+  }
+
+  const fuerzaFlash = strength.flash;
+  const fuerzaSuperman = strength.superman;
+  const fuerzaBatman = strength.batman;
+  const fuerzaAcuaman = strength.acuaman;
+
+  // Retorno de funciones
+  function activar_batiseñal(): string {
+    return "activada";
+  }
+
+  function pedir_ayuda(): void {
+    console.log("Auxilio!!!");
+  }
+
+  // Aserciones de Tipo
+  const poder: any = "100";
+  const largoDelPoder: number = (poder as string).length;
+  console.log(largoDelPoder);
 })();
