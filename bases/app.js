373363580d01fb76722923e34b437d49a8b94b8a
Tomas Cuevas <memo_mister398@hotmail.com>
2022-07-14 11:48:01 -0300

    Tarea y Resolución del ejercicio práctico #2

--- bases/app.js
+++ bases/app.js
@@ -1,35 +1,27 @@
 "use strict";
-(() => {
-    // Tipos
-    const batman = "Bruce";
-    const superman = "Clark";
-    const existe = false;
-    // Tuplas
-    const parejaHeroes = [batman, superman];
-    const villano = ["Lex Lutor", 5, true];
-    // Arreglos
-    const aliados = ["Mujer Maravilla", "Acuaman", "San", "Flash"];
-    //Enumeraciones
-    let strength;
-    (function (strength) {
-        strength[strength["acuaman"] = 0] = "acuaman";
-        strength[strength["batman"] = 1] = "batman";
-        strength[strength["flash"] = 5] = "flash";
-        strength[strength["superman"] = 100] = "superman";
-    })(strength || (strength = {}));
-    const fuerzaFlash = strength.flash;
-    const fuerzaSuperman = strength.superman;
-    const fuerzaBatman = strength.batman;
-    const fuerzaAcuaman = strength.acuaman;
-    // Retorno de funciones
-    function activar_batiseñal() {
-        return "activada";
+// Funciones Básicas
+function sumar(a, b) {
+    return a + b;
+}
+const contar = (heroes) => {
+    return heroes.length;
+};
+const superHeroes = ["Flash", "Arrow", "Superman", "Linterna Verde"];
+contar(superHeroes);
+//Parametros por defecto
+const llamarBatman = (llamar = true) => {
+    if (llamar) {
+        console.log("Batiseñal activada");
     }
-    function pedir_ayuda() {
-        console.log("Auxilio!!!");
-    }
-    // Aserciones de Tipo
-    const poder = "100";
-    const largoDelPoder = poder.length;
-    console.log(largoDelPoder);
-})();
+};
+llamarBatman();
+// Rest?
+const unirheroes = (...personas) => {
+    return personas.join(", ");
+};
+console.log(unirheroes("hola", "buenas", "goodbye", "hello"));
+// Tipo funcion
+const noHaceNada = (numero, texto, booleano, arreglo) => { };
+// Crear el tipo de funcion que acepte la funcion "noHaceNada"
+let noHaceNadaTampoco;
+noHaceNadaTampoco = noHaceNada;
