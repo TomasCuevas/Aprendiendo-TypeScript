bee97dc0e3a12e9415a24dbaa5cb3c3bc98d1769
Tomas Cuevas <memo_mister398@hotmail.com>
2022-07-15 10:40:02 -0300

    Objetos básicos

---
+++ bases/objetos/objects.js
@@ -0,0 +1,13 @@
+"use strict";
+(() => {
+    let ironman = {
+        name: "Tony Stark",
+        age: 52,
+        powers: ["volar", "super fuerza"],
+    };
+    ironman = {
+        name: "Clark Kent",
+        age: 60,
+        powers: ["super fuerza"],
+    };
+})();
---
+++ bases/objetos/objects.ts
@@ -0,0 +1,13 @@
+(() => {
+  let ironman = {
+    name: "Tony Stark",
+    age: 52,
+    powers: ["volar", "super fuerza"],
+  };
+
+  ironman = {
+    name: "Clark Kent",
+    age: 60,
+    powers: ["super fuerza"],
+  };
+})();
