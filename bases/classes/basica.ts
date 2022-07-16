893209e2896830470abc80f72713fd8d502dedca
Tomas Cuevas <memo_mister398@hotmail.com>
2022-07-16 11:46:24 -0300

    Métodos públicos y privados

--- bases/classes/basica.ts
+++ bases/classes/basica.ts
@@ -4,11 +4,22 @@
 
     constructor(
       private name: string,
-      public team: string,
+      private team: string,
       public realName?: string
     ) {}
+
+    public bio(): string {
+      return `${this.name} (${this.team})`;
+    }
+
+    static getAvgAge(): string {
+      return this.name;
+    }
   }
 
   const antman: Avenger = new Avenger("AntMan", "Capitan", "Scot Lang");
   console.log(antman);
+  console.log(antman.bio());
+
+  console.log(Avenger.getAvgAge());
 })();
--- bases/main.js
+++ bases/main.js
@@ -6,9 +6,17 @@
             this.team = team;
             this.realName = realName;
         }
+        bio() {
+            return `${this.name} (${this.team})`;
+        }
+        static getAvgAge() {
+            return this.name;
+        }
     }
     Avenger.avgAge = 35;
     const antman = new Avenger("AntMan", "Capitan", "Scot Lang");
     console.log(antman);
+    console.log(antman.bio());
+    console.log(Avenger.getAvgAge());
 })();
 //# sourceMappingURL=main.js.map
