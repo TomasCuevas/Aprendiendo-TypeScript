87c25da4aee4992c3e2265ce78d3c367ff7149fd
Tomas Cuevas <memo_mister398@hotmail.com>
2022-07-17 11:59:37 -0300

    Estructuras complejas

---
+++ bases/interfaces/complejas.ts
@@ -0,0 +1,36 @@
+(() => {
+  interface Client {
+    address?: Address;
+    age: number;
+    name: string;
+  }
+
+  interface Address {
+    city: string;
+    cp: number;
+    number: number;
+    street: string;
+  }
+
+  const client: Client = {
+    address: {
+      city: "Vera",
+      cp: 3550,
+      number: 2823,
+      street: "Belgrano",
+    },
+    age: 22,
+    name: "Tomas",
+  };
+
+  const client2: Client = {
+    address: {
+      city: "San Francisco",
+      cp: 2330,
+      number: 5320,
+      street: "Av. Street",
+    },
+    age: 20,
+    name: "Sorana",
+  };
+})();
--- bases/main.js
+++ bases/main.js
@@ -14,4 +14,26 @@
         },
     };
 })();
+(() => {
+    const client = {
+        address: {
+            city: "Vera",
+            cp: 3550,
+            number: 2823,
+            street: "Belgrano",
+        },
+        age: 22,
+        name: "Tomas",
+    };
+    const client2 = {
+        address: {
+            city: "San Francisco",
+            cp: 2330,
+            number: 5320,
+            street: "Av. Street",
+        },
+        age: 20,
+        name: "Sorana",
+    };
+})();
 //# sourceMappingURL=main.js.map
