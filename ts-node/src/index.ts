dc4984a81a6400e68731f2816f32dad96e400226
Tomas Cuevas <memo_mister398@hotmail.com>
2022-07-17 20:04:45 -0300

    Funciones Genéricas

--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -1,10 +1,16 @@
-import { printObject, genericFunction } from "./generics/generics";
+import {
+  genericFunction,
+  genericFunctionArrow,
+  printObject,
+} from "./generics/generics";
 
 printObject(123);
 printObject(new Date());
 printObject({ a: 1, b: 2, c: 3 });
 printObject([1, 2, 3, 4, 5, 6]);
-printObject("Hola Mundo");
+printObject("Un string");
 
 console.log(genericFunction(3.1416).toFixed(2));
-console.log(genericFunction("Hola Mundo").toFixed(2));
+console.log(genericFunction("Hola Mundo").toUpperCase());
+
+console.log(genericFunctionArrow("Desde la arrow function").toUpperCase());
