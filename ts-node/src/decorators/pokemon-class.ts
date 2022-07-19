81320b72f71c6a7835d470d0811e443a67c3c767
Tomas Cuevas <memo_mister398@hotmail.com>
2022-07-19 08:29:06 -0300

    Decoradores de métodos

--- ts-node/src/decorators/pokemon-class.ts
+++ ts-node/src/decorators/pokemon-class.ts
@@ -15,10 +15,32 @@ const blockPrototype = function (constructor: Function) {
   Object.seal(constructor.prototype);
 };
 
+function CheckValidPokemonId() {
+  return function (
+    target: any,
+    propertyKey: string,
+    descriptor: PropertyDescriptor
+  ) {
+    const originalMethod = descriptor.value;
+
+    descriptor.value = (id: number) => {
+      if (id < 1 || id > 800) {
+        return console.error("El ID del Pokemon debe estar entre 1 y 800");
+      }
+      return originalMethod(id);
+    };
+  };
+}
+
 @blockPrototype
 @printToConsoleConditional(true)
 export class Pokemon {
   public publicApi: string = "https://pokeapi.co";
 
   constructor(public name: string) {}
+
+  @CheckValidPokemonId()
+  savePokemonToDB(id: number) {
+    console.log(`Pokemon guardado a la base de datos. ${id}`);
+  }
 }
